4fdf9874dd1279b007b26d2b3f61fa81e182e2d2
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-10-13 13:08:31 +0200

    feat: add event table to database

--- schema.sql
+++ schema.sql
@@ -1,8 +1,19 @@
-CREATE TABLE "entries" (
-  "uid" SERIAL PRIMARY KEY,
-  "group" SMALLINT NOT NULL,
-  "firstname" VARCHAR(30) NOT NULL,
-  "surname" VARCHAR(30) NOT NULL,
-  "email" VARCHAR(50) NOT NULL,
-  "scanned" BOOLEAN NOT NULL DEFAULT FALSE
+CREATE TABLE events (
+  uid uuid NOT NULL DEFAULT gen_random_uuid(),
+  name varchar(100) NOT NULL,
+  date date NOT NULL,
+  CONSTRAINT event_pk PRIMARY KEY (uid)
 );
+
+CREATE TABLE participants (
+  uid uuid NOT NULL DEFAULT gen_random_uuid(),
+  event_uid uuid NOT NULL,
+  sciper varchar(10) NOT NULL,
+  email varchar(100) NOT NULL,
+  first_name varchar(50) NOT NULL,
+  surname varchar(50) NOT NULL,
+  CONSTRAINT participant_pk PRIMARY KEY (uid)
+);
+
+ALTER TABLE participants
+ADD CONSTRAINT participant_event_fk FOREIGN KEY (event_uid) REFERENCES public.events (uid) MATCH SIMPLE ON DELETE CASCADE ON UPDATE CASCADE;
