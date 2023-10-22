098e41461c74ce297b2bf5070ba8b0b4e0385fca
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-10-22 15:04:22 +0200

    feat: register routes

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,32 @@
-pub mod models;
+use rocket::routes;
 
-fn main() {
-    println!("Hello, world!");
+use crate::config::config;
+
+pub mod config;
+pub mod models;
+pub mod routes;
+
+type DB = rocket::State<sqlx::PgPool>;
+
+#[rocket::launch]
+async fn launch() -> _ {
+    // init config
+    config();
+
+    // Connect to database
+    let pool = sqlx::PgPool::connect(config().database_url.as_str())
+        .await
+        .unwrap();
+
+    // Launch webserver
+    rocket::build().manage(pool).mount(
+        "/",
+        routes![
+            routes::login::login,
+            routes::event::get_events,
+            routes::event::post_event,
+            routes::event::patch_event,
+            routes::event::delete_event,
+        ],
+    )
 }
