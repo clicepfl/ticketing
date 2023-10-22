3cf9283a91ccd050864e1b160c81343f660f62d0
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-10-22 15:00:14 +0200

    fix: add quote around column names

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,18 +1,24 @@
+ALTER TABLE IF EXISTS participants DROP CONSTRAINT IF EXISTS participant_event_fk;
+DROP TABLE IF EXISTS participants;
+DROP TABLE IF EXISTS events;
+
+
 CREATE TABLE events (
-  uid uuid NOT NULL DEFAULT gen_random_uuid(),
-  name varchar(100) NOT NULL,
-  date date NOT NULL,
+  "uid" uuid NOT NULL DEFAULT gen_random_uuid(),
+  "name" varchar(100) NOT NULL,
+  "date" date NOT NULL,
+  "mail_sent" boolean NOT NULL DEFAULT false,
   CONSTRAINT event_pk PRIMARY KEY (uid)
 );
 
 CREATE TABLE participants (
-  uid uuid NOT NULL DEFAULT gen_random_uuid(),
-  event_uid uuid NOT NULL,
-  sciper varchar(10) NOT NULL,
-  email varchar(100) NOT NULL,
-  first_name varchar(50) NOT NULL,
-  surname varchar(50) NOT NULL,
-  group varchar(50),
+  "uid" uuid NOT NULL DEFAULT gen_random_uuid(),
+  "event_uid" uuid NOT NULL,
+  "sciper" varchar(10) NOT NULL,
+  "email" varchar(100) NOT NULL,
+  "first_name" varchar(50) NOT NULL,
+  "surname" varchar(50) NOT NULL,
+  "group" varchar(50),
   CONSTRAINT participant_pk PRIMARY KEY (uid)
 );
 
