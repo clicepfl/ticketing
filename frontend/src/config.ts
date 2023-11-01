648732f1ff210aac13018dbaeaf1411b3c5326f8
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-11-01 10:02:00 +0100

    feat: add frontend config

---
+++ frontend/src/config.ts
@@ -0,0 +1,4 @@
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
+export const SESSION_COOKIE_NAME =
+  process.env.NEXT_PUBLIC_SESSION_COOKIE_NAME || "";
+export const ADMIN_TOKEN = process.env.ADMIN_TOKEN || "";
