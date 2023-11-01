d9de67771e8fe56e97d155ebfcabb5f7f1b314b6
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-11-01 09:58:54 +0100

    feat: add CORS header for dev environment

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,7 @@
-use rocket::{catch, catchers, http::Status, routes, serde::json::Json, Request};
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::{Header, Method};
+use rocket::{catch, catchers, http::Status, routes, serde::json::Json};
+use rocket::{Request, Response};
 use routes::Error;
 
 use crate::config::config;
@@ -17,6 +20,32 @@ fn default(status: Status, _: &Request) -> Json<Error> {
         description: None,
     })
 }
+pub struct CORS;
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
+        if std::env::var("ENVIRONMENT").is_ok_and(|s| s == "dev") {
+            response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+            response.set_header(Header::new(
+                "Access-Control-Allow-Methods",
+                "POST, GET, PATCH, OPTIONS",
+            ));
+            response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+            response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+        }
+
+        if request.method() == Method::Options {
+            response.set_status(Status::Ok);
+        }
+    }
+}
 
 #[rocket::launch]
 async fn launch() -> _ {
@@ -34,6 +63,7 @@ async fn launch() -> _ {
     // Launch webserver
     rocket::build()
         .manage(pool)
+        .attach(CORS)
         .register("/", catchers![default])
         .mount(
             config().base_uri.as_str(),
