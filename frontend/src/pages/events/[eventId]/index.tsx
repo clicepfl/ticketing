019c0f17c5d2a67a61c02fc594ff816566379a75
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-11-06 10:03:10 +0100

    feat: add style to mail buttons

--- frontend/src/pages/events/[eventId]/index.tsx
+++ frontend/src/pages/events/[eventId]/index.tsx
@@ -76,29 +76,30 @@ export default function Event({
   return (
     <div className="w-full flex justify-center">
       <div className="flex-grow flex flex-col m-8 max-w-prose text-lg items-center">
-        <h1 className="text-3xl p-5 text-center font-bold">Clicketing</h1>
+        <Link href="/">
+          <h1 className="text-3xl p-5 text-center font-bold">Clicketing</h1>
+        </Link>
 
         <div className="flex flex-col gap-4 w-full">
-          <button
-            className="text-right font-semibold origin-right hover:underline underline-offset-5 hover:text-sky-800 hover:scale-110 ease-in duration-300"
-            onClick={(e) => {
-              saveEvent();
-            }}
-          >
-            {event ? "Save" : "Create"}
-          </button>
-          <input
-            className="border-[3px] border-clic-blue rounded-lg px-2 focus:border-clic-red origin-right outline outline-0"
-            placeholder="Name"
-            defaultValue={event?.name}
-            onChange={updateField("name")}
-          />
-          <input
-            className="border-[3px] border-clic-blue rounded-lg px-2 focus:border-clic-red origin-right outline outline-0"
-            type="date"
-            defaultValue={event?.date}
-            onChange={updateField("date")}
-          />
+          <div className="flex items-center gap-6">
+            <span>Event name:</span>
+            <input
+              className="border-[3px] border-clic-blue rounded-lg px-2 focus:border-clic-red origin-right outline outline-0 grow"
+              placeholder="Name"
+              defaultValue={event?.name}
+              onChange={updateField("name")}
+            />
+          </div>
+          <div className="flex items-center gap-6">
+            <span>Event date:</span>
+            <input
+              className="border-[3px] border-clic-blue rounded-lg px-2 focus:border-clic-red origin-right outline outline-0 grow"
+              type="date"
+              defaultValue={event?.date}
+              onChange={updateField("date")}
+            />
+          </div>
+          <p>Mail template:</p>
           <Editor
             className="overflow-y-auto min-h-20 border-[3px] border-clic-blue rounded-lg px-2 focus:border-clic-red origin-right outline outline-0"
             height="70vh"
@@ -107,53 +108,86 @@ export default function Event({
             defaultValue={event?.mailTemplate}
             onChange={updateField("mailTemplate")}
           />
-        </div>
-        {event ? (
-          <>
-            <Link href={`/events/${event.uid}/checkin`}>Checkin</Link>
+          <p className="text-sm text-center">
+            The mail should be in HTML. To display the ticket, add an img tag
+            with the "qrcode" class.
+          </p>
+          {event ? (
+            <div className="flex justify-center items-center gap-8">
+              <input
+                className="border-[3px] border-clic-blue rounded-lg px-2 focus:border-clic-red origin-right outline outline-0"
+                onChange={(e) => setPreviewMailRecipient(e.target.value)}
+                placeholder="Preview email recipient"
+              />
+              <button
+                className="text-white text-center font-semibold hover:underline underline-offset-5 self-end px-2 py-1 rounded-lg bg-sky-800 hover:scale-105 ease-in duration-300 origin-right"
+                onClick={() =>
+                  (async () => {
+                    setError(null);
+
+                    let email = "";
+                    try {
+                      email =
+                        (await yup
+                          .string()
+                          .email()
+                          .required()
+                          .validate(previewMailRecipient)) || "";
+                    } catch (e: any) {
+                      setError("Recipient must be a valid email");
+                      return;
+                    }
+
+                    let res = await sendPreviewEmail(session, event.uid, email);
+                    if (typeof res === "object") {
+                      setError(res.description || "An unknown error occured");
+                    }
+                  })()
+                }
+              >
+                Send preview mail
+              </button>
+            </div>
+          ) : (
+            <></>
+          )}
+          <div className=" mr-10 flex justify-center w-full gap-6">
+            {event ? (
+              <>
+                <Link
+                  className="text-white text-2xl text-center font-semibold hover:underline underline-offset-5 self-end px-4 py-2 rounded-lg bg-pink-800 hover:scale-105 ease-in duration-300"
+                  href={`/events/${event.uid}/checkin`}
+                >
+                  Check-in page
+                </Link>
+                <button
+                  className="text-white text-2xl text-center font-semibold hover:underline underline-offset-5 self-end px-4 py-2 rounded-lg bg-sky-800 hover:scale-105 ease-in duration-300"
+                  disabled={event.mailSent}
+                  onClick={() => {
+                    if (confirm("Are you sure you want to send the mails ?")) {
+                      sendEmails(session, event.uid).then(() =>
+                        router.reload()
+                      );
+                    }
+                  }}
+                >
+                  Send mail
+                </button>
+              </>
+            ) : (
+              <></>
+            )}
             <button
-              disabled={event.mailSent}
-              onClick={() => {
-                sendEmails(session, event.uid).then(() => router.reload());
+              className="text-white text-center text-2xl font-semibold hover:underline underline-offset-5 self-end mr-5 px-4 py-2 rounded-lg bg-green-600 hover:scale-110 ease-in duration-300"
+              onClick={(e) => {
+                saveEvent();
               }}
             >
-              Send mail
+              {event ? "Save" : "Create"}
             </button>
-            <input
-              onChange={(e) => setPreviewMailRecipient(e.target.value)}
-              placeholder="Preview email recipient"
-            />
-            <button
-              onClick={() =>
-                (async () => {
-                  setError(null);
+          </div>
+        </div>
 
-                  let email = "";
-                  try {
-                    email =
-                      (await yup
-                        .string()
-                        .email()
-                        .required()
-                        .validate(previewMailRecipient)) || "";
-                  } catch (e: any) {
-                    setError("Recipient must be a valid email");
-                    return;
-                  }
-
-                  let res = await sendPreviewEmail(session, event.uid, email);
-                  if (typeof res === "object") {
-                    setError(res.description || "An unknown error occured");
-                  }
-                })()
-              }
-            >
-              Send preview mail
-            </button>
-          </>
-        ) : (
-          <></>
-        )}
         {error ? <p>{error}</p> : <></>}
       </div>
     </div>
