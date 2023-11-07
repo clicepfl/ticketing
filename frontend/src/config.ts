ce3f8c7989bb7683d1e859dd784e2fe5fa663b46
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-11-07 09:11:25 +0100

    fix: api url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,7 +1,7 @@
 // Environment variables are not available to the frontend in production mode. They must be hard-coded.
 
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "https://clic.epfl.ch";
+  process.env.NEXT_PUBLIC_API_URL || "https://clic.epfl.ch/clicketing/api";
 export const SESSION_COOKIE_NAME =
   process.env.NEXT_PUBLIC_SESSION_COOKIE_NAME || "clicketing-session";
 export const ADMIN_TOKEN = process.env.ADMIN_TOKEN || "";
